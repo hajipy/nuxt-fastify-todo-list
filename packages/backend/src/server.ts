779ba8cb689932be7eafb94b51acc37c30b6a67f
Kenichi Saito <hajime@studiohff.net>
2021-06-15 23:07:03 +0900

    APIサーバでPOSTを受け付けたら新規TODOを作る

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,9 +1,26 @@
 import Fastify, { FastifyInstance } from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 const server: FastifyInstance = Fastify({ logger: true });
 
+const prisma = new PrismaClient();
+
 server.get("/", async (request, reply) => {
     return { hello: "world" };
+});
+
+server.post<{
+    Body: { title: string; },
+}>("/todo", async (request, repay) => {
+    console.log(request.body);
+
+    const newTodo = await prisma.todo.create({
+        data: {
+            title: request.body.title || "new task",
+        },
+    });
+
+    return { todo: newTodo };
 });
 
 const startServer = async () => {
