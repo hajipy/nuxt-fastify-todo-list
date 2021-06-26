b1e6a4c93f4119ca5d0f772e4617ea8efdcfd8cb
Kenichi Saito <hajime@studiohff.net>
2021-06-27 02:38:45 +0900

    Todo追加を実装

--- packages/frontend/pages/index.vue
+++ packages/frontend/pages/index.vue
@@ -1,16 +1,33 @@
 <template>
-    <ul>
-        <li v-for="todo in todos">{{ todo.title }}</li>
-    </ul>
+    <div>
+        <ul>
+            <li v-for="todo in todos">{{ todo.title }}</li>
+        </ul>
+
+        <form v-on:submit="onSubmit">
+            <input v-model="title" autofocus />
+        </form>
+    </div>
 </template>
 
 <script lang="ts">
     import Vue from "vue";
 
     export default Vue.extend({
+        data() {
+            return {
+                title: "",
+            };
+        },
         computed: {
             todos() {
                 return this.$store.state.todos;
+            },
+        },
+        methods: {
+            async onSubmit() {
+                const todo = await this.$nuxt.context.$axios.$post("/api/todo", { title: this.title });
+                this.$store.commit("addTodos", [todo]);
             },
         },
         async fetch() {
